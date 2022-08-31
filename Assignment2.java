79235d5ebca297492dce7fb538f51099dda41197
Sherwin Wang <sherwinnw22@gmail.com>
2022-08-31 10:40:53 -0700

    First Attempt at Assignement2 '

--- Assignment2.java
+++ Assignment2.java
@@ -10,5 +10,32 @@ public class Assignment2 {
     public static void main (String[] args){
        Scanner console = new Scanner(System.in);
 
+       int number = console.nextInt();
+       int countOdd = 0;
+       int minInt = Integer.MAX_VALUE;
+       int largestEven = Integer.MIN_VALUE;
+       int sumPositive = 0;
+       while(number != 0){
+            if(number < minInt){
+                minInt = number;
+            }
+            if(number > 0){
+                sumPositive += number;
+                if(number % 2 == 0){
+                    if(number > largestEven){
+                        largestEven = number;
+                    }
+                }
+                else{
+                    countOdd ++;
+                }
+            }
+            number = console.nextInt();
+       }
+
+       System.out.print("The minimum integer is " + minInt +
+                        "\nThe count of odd integers in the sequence is " + countOdd +
+                        "\nThe largest even integer in the sequence is " + largestEven +
+                        "\nThe sum of positive integers is " + sumPositive);
     }
 }
