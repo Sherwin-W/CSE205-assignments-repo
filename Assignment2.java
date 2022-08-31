c4dac361d95aedca0555ec2c55f4a77b5eafeeb6
Sherwin Wang <sherwinnw22@gmail.com>
2022-08-31 10:17:39 -0700

    added assignment2

---
+++ Assignment2.java
@@ -0,0 +1,14 @@
+// Assignment #: 1
+//         Name: Sherwin Wang
+//    StudentID: 1224486887
+//      Lecture: M W F 10:10 AM - 11:00 AM
+//  Description: 
+
+import java.util.Scanner;  // use the Scanner class located in the "java.util" directory
+
+public class Assignment2 {
+    public static void main (String[] args){
+       Scanner console = new Scanner(System.in);
+
+    }
+}
