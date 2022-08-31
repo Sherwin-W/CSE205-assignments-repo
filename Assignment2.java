90eb4304dc89a71cd95dbd7316a36413613f28f2
Sherwin Wang <sherwinnw22@gmail.com>
2022-08-31 10:52:59 -0700

    Finalized Assignment2

--- Assignment2.java
+++ Assignment2.java
@@ -2,7 +2,7 @@
 //         Name: Sherwin Wang
 //    StudentID: 1224486887
 //      Lecture: M W F 10:10 AM - 11:00 AM
-//  Description: 
+//  Description: Reads a sequence of integers and outputs the minimum integer, count of odd integers, largest even integer, and the sum of positive integers.
 
 import java.util.Scanner;  // use the Scanner class located in the "java.util" directory
 
