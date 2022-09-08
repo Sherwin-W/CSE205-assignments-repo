4fa35d11c395e40e710882281b81a4cc902bccf2
Sherwin Wang <sherwinnw22@gmail.com>
2022-09-08 08:22:14 -0700

    fixed assignement3 text

--- Assignment3.java
+++ Assignment3.java
@@ -6,26 +6,26 @@
 
 public class Assignment3 {
 	
-Principle 2: CLIENT AND EMPLOYER – "Software engineers shall act in a manner that is in the best interests of their client and employer consistent with the public interest."
-                            Example: A software engineer is hired to solve a problem with a company's website. A competitor company gets wind of this and pays the engineer to purposefully write code that
-                                    doesn't work. The engineer accepts and sabotages the company's website. This would not be acting in a manner with the best interests of their first client.
+// Principle 2: CLIENT AND EMPLOYER – "Software engineers shall act in a manner that is in the best interests of their client and employer consistent with the public interest."
+//                             Example: A software engineer is hired to solve a problem with a company's website. A competitor company gets wind of this and pays the engineer to purposefully write code that
+//                                     doesn't work. The engineer accepts and sabotages the company's website. This would not be acting in a manner with the best interests of their first client.
 
-Principle 4: JUDGMENT – "Software engineers shall maintain integrity and independence in their professional judgment."
-                            Example: A software engineer is brought to solve a problem. They notice that the problem is similar to a problem their friend is working on. They ask their friend for 
-                                    some help and that friend shows them their code. The engineer ends up copying most of their friend's code and makes some minor changes due to the minor difference 
-                                    in problems. When there's errors, the engineer asks stackoverflow for solutions and hires someone to look over the code. This would not maintain integrity.
+// Principle 4: JUDGMENT – "Software engineers shall maintain integrity and independence in their professional judgment."
+//                             Example: A software engineer is brought to solve a problem. They notice that the problem is similar to a problem their friend is working on. They ask their friend for 
+//                                     some help and that friend shows them their code. The engineer ends up copying most of their friend's code and makes some minor changes due to the minor difference 
+//                                     in problems. When there's errors, the engineer asks stackoverflow for solutions and hires someone to look over the code. This would not maintain integrity.
 
-Principle 5: MANAGEMENT – "Software engineering managers and leaders shall subscribe to and promote an ethical approach to the management of software development and maintenance."
-                            Example: A company wants to complete a project in a short span of time. It employs a team of software engineers to solve the problem, which is unsolveable in such a 
-                                    short time crunch. The engineers are forced to work 22 hour workdays in order to complete the project and not get fired. This would not be ethical management.
+// Principle 5: MANAGEMENT – "Software engineering managers and leaders shall subscribe to and promote an ethical approach to the management of software development and maintenance."
+//                             Example: A company wants to complete a project in a short span of time. It employs a team of software engineers to solve the problem, which is unsolveable in such a 
+//                                     short time crunch. The engineers are forced to work 22 hour workdays in order to complete the project and not get fired. This would not be ethical management.
 
-Principle 7: COLLEAGUES – "Software engineers shall be fair to and supportive of their colleagues."
-                            Example: A team of software engineers are working on the same project. One of them cares much more than the others, so they do the project themselves while the others 
-                                    make excuses on why they cannot help in the project. After the project is submitted, the others take credit for the project. This wouldn't be fair to their colleague who did everything.
+// Principle 7: COLLEAGUES – "Software engineers shall be fair to and supportive of their colleagues."
+//                             Example: A team of software engineers are working on the same project. One of them cares much more than the others, so they do the project themselves while the others 
+//                                     make excuses on why they cannot help in the project. After the project is submitted, the others take credit for the project. This wouldn't be fair to their colleague who did everything.
 
-Principle 8: SELF – "Software engineers shall participate in lifelong learning regarding the practice of their profession and shall promote an ethical approach to the practice of the profession."
-                            Example: A software engineer works hard and lands a position at a large company. Their salary is enough for them to live a financial burden free life, so they stop trying as hard to improve.
-                                    Instead, they use their money to pay others to do his job for him secretly. This would not be eithical nor would it show learning.
+// Principle 8: SELF – "Software engineers shall participate in lifelong learning regarding the practice of their profession and shall promote an ethical approach to the practice of the profession."
+//                             Example: A software engineer works hard and lands a position at a large company. Their salary is enough for them to live a financial burden free life, so they stop trying as hard to improve.
+//                                     Instead, they use their money to pay others to do his job for him secretly. This would not be eithical nor would it show learning.
 
 
 
