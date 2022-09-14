9f7ca3e7f64de4ce60f0e5c458ee9cf3e52899df
Sherwin Wang <sherwinnw22@gmail.com>
2022-09-14 12:30:00 -0700

    third fix

--- CombatHelicopter.java
+++ CombatHelicopter.java
@@ -37,7 +37,7 @@ public class CombatHelicopter extends AircraftEntity{
     }
 
     public String toString(){
-        return "Combat Helicopter:\nAircraft name:\t" + this.entityName + "\nCurrent Ammo:\t" + 
+        return "Combat Helicopter\nAircraft name:\t" + this.entityName + "\nCurrent Ammo:\t" + 
                 this.ammo + "\nRange:\t" + this.range + "\nCurrent Attack Power:\t" + 
                 this.attackPower + "\nStealth Index:\t" + this.stealthIndex + "\n";
     }
